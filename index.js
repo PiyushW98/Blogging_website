7aad7f61722502c3f1f5113abcf9c1696babd990
PiyushW98 <wasekarpiyush98@gmail.com>
2024-04-15 14:08:58 +0530

    first video done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const PORT = 8000;
 const express = require('express');
+const path = require('path');
+//acquiring the user model 
+const mongoose = require('mongoose');
+
+const UserRoute = require("./routes/user");
+
+const PORT = 8000;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/blogApp').then(() => console.log('mongoose connected'));
+
+//we are using EJS for server side rendering 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.use('/user', UserRoute);
+
+app.listen(PORT, () => {
+    console.log(`server has started at http://localhost:${PORT}/`);
+})
---
+++ readMe.md
@@ -0,0 +1,21 @@
+$install dependencies :
+[1] Dependencies
+
+> express (npm i express)
+> EJS (npm i ejs) : For server side rendering
+> mongoose (npm i mongoose) : For connecting with mongo DB
+
+[2] dev Dependencies
+
+> nodemon (npm i nodemon)
+
+$models :
+[1] user.js :
+
+> designing the schema for the user
+> for hasing purpose we will use crypto(built-in) module
+
+$routes :
+[1] user.js :
+
+> made routes for GET signin, GET signup, POST signup
